e45d471d58da9c9d519981a7fcdabc313adc030f
Shane Floresca <floresca.shanegarcia@gmail.com>
2025-10-26 21:47:44 +0800

    Build User Controller with Get All Users API Endpoint

--- src/main/java/io/github/shnflrsc/picoims/controller/UserController.java
+++ src/main/java/io/github/shnflrsc/picoims/controller/UserController.java
@@ -1,10 +1,28 @@
 package io.github.shnflrsc.picoims.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/users")
+import io.github.shnflrsc.picoims.model.User;
+import io.github.shnflrsc.picoims.repository.UserRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
     
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
