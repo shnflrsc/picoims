b009c6e9505c6fc40ce843f911bc973f4803e8fa
Shane Floresca <floresca.shanegarcia@gmail.com>
2025-10-31 12:44:16 +0800

    Define Transaction Controller and define get all transactions endpoint

--- src/main/java/io/github/shnflrsc/picoims/controller/TransactionController.java
+++ src/main/java/io/github/shnflrsc/picoims/controller/TransactionController.java
@@ -1,10 +1,27 @@
 package io.github.shnflrsc.picoims.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
-@RequestMapping("/transactions")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.shnflrsc.picoims.model.Transaction;
+import io.github.shnflrsc.picoims.repository.TransactionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/transactions")
+@CrossOrigin(origins = "*")
 public class TransactionController {
+    
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
     
 }
