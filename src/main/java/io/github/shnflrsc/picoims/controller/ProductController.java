e3b362355f42f603a2dbfb3e3b4f8b19c20e8b58
Shane Floresca <floresca.shanegarcia@gmail.com>
2025-10-31 12:44:01 +0800

    Define Product Controller and GET all products endpoint

--- src/main/java/io/github/shnflrsc/picoims/controller/ProductController.java
+++ src/main/java/io/github/shnflrsc/picoims/controller/ProductController.java
@@ -1,10 +1,26 @@
 package io.github.shnflrsc.picoims.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
-@RequestMapping("/products")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.shnflrsc.picoims.model.Product;
+import io.github.shnflrsc.picoims.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
     
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
